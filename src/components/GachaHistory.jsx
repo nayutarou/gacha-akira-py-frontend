b93f59b08a7192804dbdb4344a82b8237252ac36
akira <a.oono.sys23@morijyobi.ac.jp>
2025-07-23 10:10:23 +0900

    第n回という表示からn回というものに修正

--- src/components/GachaHistory.jsx
+++ src/components/GachaHistory.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 const GachaHistory = ({ history }) => {
   return (
-    <div className="gacha-history-container mt-8 p-4 bg-white bg-opacity-20 rounded-lg shadow-lg max-h-60 overflow-y-auto">
-      <h2 className="text-xl font-bold text-gray-800 mb-4">ガチャ履歴</h2>
+    <div className="p-4 mt-8 overflow-y-auto bg-white rounded-lg shadow-lg gacha-history-container bg-opacity-20 max-h-60">
+      <h2 className="mb-4 text-xl font-bold text-gray-800">ガチャ履歴</h2>
       {history.length === 0 ? (
         <p className="text-gray-800">まだ履歴はありません。</p>
       ) : (
         <ul className="space-y-2">
           {history.map((item, index) => (
             <li key={index} className={`text-${item.result} text-lg`}>
-              <span className="font-semibold">第{history.length - index}回: {item.result}</span>
+              <span className="font-semibold">{history.length - index}回目: {item.result}</span>
             </li>
           ))}
         </ul>
