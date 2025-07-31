32a65adcfa922aeb205d128cd69bfec2b54660d5
akira <a.oono.sys23@morijyobi.ac.jp>
2025-07-31 12:01:20 +0900

    docs(main): main.jsxに日本語コメントを追加

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,19 @@ import HistoryPage from "./pages/HistoryPage.jsx";
 import { GachaProvider } from "./GachaContext.jsx";
 import "./styles.css";
 
+// Reactアプリケーションのエントリーポイント
 ReactDOM.createRoot(document.getElementById("root")).render(
+  // 開発時の厳格なチェックを有効にする
   <React.StrictMode>
+    {/* ブラウザのURLとReact Routerを同期させる */}
     <BrowserRouter>
+      {/* ガチャの状態管理をアプリケーション全体に提供する */}
       <GachaProvider>
+        {/* ルーティング設定 */}
         <Routes>
+          {/* ルートパス ("/") にAppコンポーネントを割り当てる */}
           <Route path="/" element={<App />} />
+          {/* "/history" パスにHistoryPageコンポーネントを割り当てる */}
           <Route path="/history" element={<HistoryPage />} />
         </Routes>
       </GachaProvider>
